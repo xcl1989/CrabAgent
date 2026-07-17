cf261a24e48ddde43fddced17574fa7311396508
xcl1989 <xcl1989@live.com>
2026-07-17 12:10:50 +0800

    chore: update frontend assets

---
+++ src/crabagent/static/assets/swimlanesDiagram-G3AALYLV-CkSgpu5M.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U-Cxf2bwYX.js";import{a}from"./mermaid.core-CwayTbWL.js";import"./chunk-5VM5RSS4-ChdWcQxB.js";import"./chunk-XXDRQBXY-BbMMapZX.js";import"./chunk-VR4S4FIN-rI3peoUo.js";import"./chunk-32BRIVSS-C9ZCcBHW.js";import"./channel-ZEv0shda.js";import"./index-02QdknRG.js";import"./vendor-react-BEFBu0qs.js";import"./vendor-ui-DQ-ZhZpk.js";import"./vendor-charts-Br5QTkJr.js";import"./vendor-markdown-C0jI_Wfn.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,v=r({defaultLayout:"swimlane",styles:m});export{v as diagram};
--- src/crabagent/static/assets/swimlanesDiagram-G3AALYLV-DV_cZt1S.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U-BgNlkKZY.js";import{a}from"./mermaid.core-fwRuKLA0.js";import"./chunk-5VM5RSS4-BsdNJ1Ix.js";import"./chunk-XXDRQBXY-CmUSQaXj.js";import"./chunk-VR4S4FIN-CBk6pava.js";import"./chunk-32BRIVSS-DrW65FTf.js";import"./channel-CnmXXz3q.js";import"./index-ROQVyJW7.js";import"./vendor-react-BEFBu0qs.js";import"./vendor-ui-DQ-ZhZpk.js";import"./vendor-charts-Br5QTkJr.js";import"./vendor-markdown-C0jI_Wfn.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,v=r({defaultLayout:"swimlane",styles:m});export{v as diagram};
